354245216ea328e951f44bed56347320d4c1753c
Lívia Macedo <125308991+LiviaMacc@users.noreply.github.com>
2026-07-16 00:31:22 +0000

    enviando o arquivo aula3_4.py

---
+++ aula3_4.py
@@ -0,0 +1,15 @@
+"""
+Comandos Git
+
+git init - inicia o diretório atual como git (versionamento conjunto de máquinas)
+git status - verifica se há alguma alteração na sua brach que não tem na merge
+git add aquivo.py - prepara para enviar o que foi alterado da merge
+git commit -m "Correção do 4° laço de repetição" - envia para a área de transferência a nova versão / altera a merge
+cd .. - voltando para o diretório anterior
+git push -
+git clone - 
+git pull - 
+git branch -M main - 
+git remote add origin git@github.com:LiviaMacc/Python-SENAC.git - 
+git push -u origin main - 
+"""
